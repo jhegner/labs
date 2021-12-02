3189fc0fbb17f4f46601d7580c43b987469c02db
jhegner <jidlafehegner@gmail.com>
2021-12-02 00:15:32 -0300

    chore: adicionado consulta a lista de filas sqs

--- messaging/sqs/README.txt
+++ messaging/sqs/README.txt
@@ -1,4 +1,7 @@
 # SQS Simple Example
 
-## API
+## API SDK SQS
 https://docs.aws.amazon.com/cli/latest/reference/sqs/index.html
+
+## Spring Cloud SQS Messaging
+https://cloud.spring.io/spring-cloud-aws/spring-cloud-aws.html#_messaging
--- messaging/sqs/src/main/java/com/br/jhegnerlabs/App.java
+++ messaging/sqs/src/main/java/com/br/jhegnerlabs/App.java
@@ -1,8 +1,19 @@
 package com.br.jhegnerlabs;
 
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.ListQueuesResult;
+
 public class App {
 
     public static void main(String[] args) {
+
+        AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+        ListQueuesResult lqResult = sqs.listQueues();
+        System.out.println("Lista de Filas SQS configuradas");
+        for(String url : lqResult.getQueueUrls()) {
+            System.out.println(url);
+        }
 
     }
 
