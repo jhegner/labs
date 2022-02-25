f1c8f56c490c734ca0ed777a58b469c47196c43d
jhegner <jidlafehegner@gmail.com>
2022-02-25 09:46:08 -0300

    removido import nao utilizado

--- test/lab-test-junit5-argument-source/src/test/java/br/com/jhegnerlabs/junit5/AppCsvFileSourceTest.java
+++ test/lab-test-junit5-argument-source/src/test/java/br/com/jhegnerlabs/junit5/AppCsvFileSourceTest.java
@@ -1,6 +1,5 @@
 package br.com.jhegnerlabs.junit5;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
