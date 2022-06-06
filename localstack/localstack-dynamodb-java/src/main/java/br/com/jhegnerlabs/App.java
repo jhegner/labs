6c797da5f9d6b362b3090cb1fcf4f81a9be10e1c
jhegner <jidlafehegner@gmail.com>
2022-06-06 02:27:43 -0300

    chore: adicionado chamada para o Repositorio com chamada local

--- localstack/localstack-dynamodb-java/src/main/java/br/com/jhegnerlabs/App.java
+++ localstack/localstack-dynamodb-java/src/main/java/br/com/jhegnerlabs/App.java
@@ -1,13 +1,11 @@
 package br.com.jhegnerlabs;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import br.com.jhegnerlabs.dynamodb.repository.Repository;
+
+public class App {
+    public static void main(String[] args) {
+        new Repository().consultaProcesso(
+                "e86fcdfb-c200-4737-8b1c-7923e25e0843",
+                "9817be8b-309c-417f-8ff9-fac96655a937");
     }
 }
--- localstack/localstack-dynamodb-java/src/main/java/br/com/jhegnerlabs/dynamodb/repository/Repository.java
+++ localstack/localstack-dynamodb-java/src/main/java/br/com/jhegnerlabs/dynamodb/repository/Repository.java
@@ -1,16 +1,52 @@
 package br.com.jhegnerlabs.dynamodb.repository;
 
 import br.com.jhegnerlabs.dynamodb.data.Processo;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Map;
 
+@Slf4j
 public class Repository {
+
+    public static final String SERVICE_ENDPOINT = "http://localhost:4566";
 
     public Processo consultaProcesso(String idCliente, String idProcesso) {
 
-        Region region = Region.SA_EAST_1;
+        var endpointConfig = new AwsClientBuilder.EndpointConfiguration(
+                SERVICE_ENDPOINT,
+                Regions.US_EAST_1.getName()
+        );
 
-        
+//        var client = AmazonDynamoDBClient.builder()
+//                .withEndpointConfiguration(endpoint)
+//                .withRegion(Regions.SA_EAST_1).build();
+
+            var client = AmazonDynamoDBClientBuilder
+                    .standard()
+                    .withEndpointConfiguration(endpointConfig).build();
+
+        var request = new GetItemRequest()
+                .withTableName("TB_CONTROLE_PROCESSO_PESSOA_JURIDICA")
+                .withKey(Map.of(
+                        "IdPessoaJuridica", new AttributeValue().withS(idCliente),
+                        "SK", new AttributeValue().withS("PROCESSO#" + idProcesso)));
+
+        try {
+
+            var result = client.getItem(request);
+            log.info("Processo - {}", result);
+
+        } catch (Exception ex) {
+           log.error("Erro ao consultar base do DynamoDB", ex);
+        }
+
         return new Processo();
     }
-    
+
 }
