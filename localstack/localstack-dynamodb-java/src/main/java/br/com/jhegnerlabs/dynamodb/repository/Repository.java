26b302c87192bca01287277c1667a1ba1d90cc44
jhegner <jidlafehegner@gmail.com>
2022-06-05 23:21:33 -0300

    chore: ajustado dependencias no pom e outras melhorias

---
+++ localstack/localstack-dynamodb-java/src/main/java/br/com/jhegnerlabs/dynamodb/repository/Repository.java
@@ -0,0 +1,16 @@
+package br.com.jhegnerlabs.dynamodb.repository;
+
+import br.com.jhegnerlabs.dynamodb.data.Processo;
+
+
+public class Repository {
+
+    public Processo consultaProcesso(String idCliente, String idProcesso) {
+
+        Region region = Region.SA_EAST_1;
+
+        
+        return new Processo();
+    }
+    
+}
